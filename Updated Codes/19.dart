a8bd131cc7054be9a18df6311a2e537b7cbc3410
Waqar Ali Siyal <49365563+siraiwaqarali@users.noreply.github.com>
2022-03-20 19:19:35 +0500

    Update 19.dart

--- Updated Codes/19.dart
+++ Updated Codes/19.dart
@@ -1,1 +1,24 @@
-void main() {}
+/*
+ * Required Parameters
+ * Optional Parameters
+ *   1) Positional  2) Named  3) Default
+ */
+void main() {
+  printCities("Karachi", "Jamshoro", "Islamabad");
+  print("");
+  printCountries("Pakistan", "China");
+}
+
+// Required Parameters
+void printCities(String name1, String name2, String name3){
+  print("Name1 is $name1");
+  print("Name2 is $name2");
+  print("Name3 is $name3");
+}
+
+// Optional Positional Parameters
+void printCountries(String name1, [String? name2, String? name3]){
+  print("Name1 is $name1");
+  print("Name2 is $name2");
+  print("Name3 is $name3");
+}
